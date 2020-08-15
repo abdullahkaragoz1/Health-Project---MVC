47a6ba2840a98df524abf5b1c5514278cda584cc
Abdullah Karagöz <abdullahkaragoz30@gmail.com>
2020-08-15 19:19:35 +0300

    App Added.

---
+++ Health Project/HEALTH/Controllers/HealthController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HEALTH.Controllers
+{
+    public class HealthController : Controller
+    {
+        HEALTHEntities db = new HEALTHEntities();
+        // GET: Health
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+      
+
+    }
+}
---
+++ Health Project/HEALTH/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HEALTH.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult Records()
+        {
+            return View();
+        }
+
+        public ActionResult Information()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult SignIn()
+        {
+            return View();
+        }
+        public ActionResult GetRecords()
+        {
+            using (HEALTHEntities _db = new HEALTHEntities())
+            {
+                var records = (from d1 in _db.Records
+                               select new
+                               {
+                                   Id = d1.Id,
+                                   FirstName = d1.FirstName,
+                                   LastName = d1.LastName,
+                                   Age = d1.Age,
+                                   Gender = d1.Gender,
+                                   BloodGroup = d1.BloodGroup,
+                                   EmailAdress = d1.EmailAdress,
+                                   RecordDescription = d1.RecordDescription
+                               }).ToList();
+                return Json(new { data = records }, JsonRequestBehavior.AllowGet);
+            }
+
+        }
+    }
+}
